966f86bbd530510cca022ea10cf0efe98cdb6347
Claudio <claudio.sousa@gmail.com>
2016-11-22 19:59:33 +0100

    dump modules

---
+++ src/gol_worker.h
@@ -0,0 +1,4 @@
+struct gol_worker_t;
+typedef struct gol_worker_t gol_worker_t;
+gol_worker_t * gol_worker_start(gol_t *gol_grid, barrier_workers, barrier_display, bool *working);
+void gol_worker_stop(gol_worker_t * gol_worker);
